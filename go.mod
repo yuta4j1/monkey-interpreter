ceb9f54c5a8ec2fd86fb52d11e628ac0991ea96a
yuta4j1 <kascado.ys10@gmail.com>
2020-09-17 12:05:52 +0900

    ざっくり字句解析

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yuta4j1/monkey-interpreter
+
+go 1.14
--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,6 @@
 package lexer
 
-import token "github.com/yuta4j1/monkey_interpreter/token"
+import "github.com/yuta4j1/monkey-interpreter/token"
 
 type Lexer struct {
 	input        string
@@ -15,6 +15,7 @@ func New(input string) *Lexer {
 	return l
 }
 
+// 一文字ずつ読む
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -25,23 +26,83 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+// 空白をスキップする
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	// 空白文字は字句解析の対象にならない
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
-		tok := newToken(token.ASSIGN, l.ch)
+		tok = newToken(token.ASSIGN, l.ch)
 	case ';':
-		tok = newToken(token.SEMICORON, l.ch)
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
 }
 
+// 識別子を読み、非英字に到達するまで字句解析の位置を進める
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 識別子（数値）を読みこむ
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
